abc144973b838b9be6f2eef8a5b6391a026f91b0
Stefan Clinciu <Stefan Clinciu>
2018-09-08 11:55:23 +0200

    Add project files.

---
+++ App1/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.Threading;
+
+namespace App1
+{
+    [Activity(Label = "Login", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        private Button btnSignUp;
+        private Button btnSignIn;
+        private ProgressBar mProgressBar;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            btnSignUp = FindViewById<Button>(Resource.Id.SignUp);
+            btnSignIn = FindViewById<Button>(Resource.Id.SignIn);
+            mProgressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            
+            btnSignUp.Click += btnSignUp_Click;
+            btnSignIn.Click += BtnSignIn_Click;
+
+
+        }
+
+        private void BtnSignIn_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void btnSignUp_Click(object sender, EventArgs e)
+        {
+            FragmentTransaction transaction = FragmentManager.BeginTransaction();
+            dialog_SignUp signUpDialog = new dialog_SignUp();
+            signUpDialog.Show(transaction, "dialog fragment");
+            
+
+            signUpDialog.MOnSignUpComplete += signUpDialog_MOnSignUpComplete;
+        }
+
+         void signUpDialog_MOnSignUpComplete(object sender, OnSignUpEventArgs e)
+        {
+            mProgressBar.Visibility = Android.Views.ViewStates.Visible;
+            Thread thread = new Thread(serverLike);
+            thread.Start();
+            
+        }
+
+         void serverLike()
+        {
+            Thread.Sleep(5000);
+            RunOnUiThread(() => { mProgressBar.Visibility = Android.Views.ViewStates.Invisible; });
+        }
+    }
+}
+
---
+++ App1/dialog_SignUp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    public class OnSignUpEventArgs : EventArgs
+    {
+        private string mFirstName;
+        private string mEmail;
+        private string mPassword;
+
+        public string FirstName
+        {
+            get { return mFirstName; }
+            set { mFirstName = value; }
+        }
+        public string Email
+        {
+            get { return mEmail; }
+            set { mEmail = value; }
+        }
+        public string Password
+        {
+            get { return mPassword; }
+            set { mPassword = value; }
+        }
+
+        public OnSignUpEventArgs(string firstName, string email, string password) : base()
+        {
+            FirstName = firstName;
+            Email = email;
+            Password = password;
+        }
+
+
+
+    }
+    class dialog_SignUp : DialogFragment
+    {
+        private EditText txtFirstName;
+        private EditText txtEmail;
+        private EditText txtPassword;
+        private Button btnSignUp;
+
+        public event EventHandler<OnSignUpEventArgs> MOnSignUpComplete;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.dialog, container, false);
+
+            txtFirstName = view.FindViewById<EditText>(Resource.Id.txtFirstName);
+            txtEmail = view.FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+            btnSignUp = view.FindViewById<Button>(Resource.Id.btnDialogEmail);
+
+            btnSignUp.Click += BtnSignUp_Click;
+            return view;
+        }
+
+         void BtnSignUp_Click(object sender, EventArgs e)
+        {
+            //User has clicked the sign up button
+            MOnSignUpComplete.Invoke(this, new OnSignUpEventArgs(txtFirstName.Text, txtEmail.Text, txtPassword.Text));
+            this.Dismiss();
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation;
+        }
+    }
+}
